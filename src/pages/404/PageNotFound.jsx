cfbc22bbeb1edc7d36b463a01ad596f0a65cc038
kousik maity <kousik@dreamzinteractive.com>
2023-09-24 00:22:24 +0530

    error page create

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+  import React from "react";
 
-const PageNotFound = () => {
-  return (
-    <div>PageNotFound</div>
-  )
-}
-
-export default PageNotFound
+  import "./style.scss";
+  import ContentWrapper from "../../components/contentwrapper/ContentWrapper"
+  const PageNotFound = () => {
+      return (
+          <div className="pageNotFound">
+                    <ContentWrapper>
+                  <span className="bigText">404</span>
+                  <span className="smallText">Page not found!</span>
+              </ContentWrapper>
+          </div>
+      );
+  };
+  
+  export default PageNotFound;
